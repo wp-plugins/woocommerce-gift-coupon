884f82c30e0ecc197cfb31c61dc09ce10e40dc6a
studiosweb <studiosweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-12 08:44:19 +0000

    Add new version 1.1 and initial release
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-gift-coupon/tags/1.1@1158615 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,19 @@
+<?php
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    exit;
+}
+
+global $wpdb;
+
+delete_option( 'woocommerce_gift_coupon_text_color_header' );
+delete_option( 'woocommerce_gift_coupon_text_color_footer' );
+delete_option( 'woocommerce_gift_coupon_text_color_title' );
+delete_option( 'woocommerce_gift_coupon_bg_color_header' );
+delete_option( 'woocommerce_gift_coupon_bg_color_footer' );
+delete_option( 'woocommerce_gift_coupon_info_paragraph' );
+delete_option( 'woocommerce_gift_coupon_subject' );
+delete_option( 'woocommerce_gift_coupon_title' );
+
+$table = $wpdb->prefix . "woocommerce_gift_coupon";
+$sql = 'DROP TABLE '.$table;
+$wpdb->query( $sql );
