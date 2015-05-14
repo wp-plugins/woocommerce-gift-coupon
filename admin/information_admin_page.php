946bb33a7f371ea2abcf3aef69d7bc93b6210f60
studiosweb <studiosweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-14 19:03:23 +0000

    Fixed color picker email
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-gift-coupon/tags/1.5@1160709 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/information_admin_page.php
@@ -0,0 +1,10 @@
+<div class="wrap">
+    <div id="woocommerce-gift-coupon">
+        <?php _e( '<h2>Woocommerce Gift Coupon - Documentation</h2>', 'woocommerce-gift-coupon' ); ?>
+        <div id="gs-middle">
+            <?php require_once( WOOCOMMERCE_GIFT_COUPON_DIR."admin/includes/gs-middle-information-page.php" ); ?>
+        </div>
+    </div>
+</div>
+
+
