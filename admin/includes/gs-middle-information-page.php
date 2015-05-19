9c461c07391c2e560a87e88f94f23ba8d4665de6
studiosweb <studiosweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-19 07:02:16 +0000

    Version 1.6 - Fixed: Author URL
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-gift-coupon/tags/1.6@1163119 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/includes/gs-middle-information-page.php
@@ -0,0 +1,39 @@
+<div class="container">
+    <div class="block" class="getting-started">
+        <div class="wgc-box">
+            <div class="header medium">
+                <?php _e( '<h4>Getting started</h4>', 'woocommerce-gift-coupon' ); ?>
+            </div>
+            <div class="wgc-box-body">
+                <?php _e( 'Woocommerce Gift Coupon lets you create a WooCommerce product category known as GiftCoupon which will serve as a reference for our customers to give discount coupons to friends in future purchases . When buying this type of product , the administrator comes the order type GiftCoupon and when complete, the administrator can generate coupons and codes to send mail to the consumer.', 'woocommerce-gift-coupon' ); ?>
+            </div>
+        </div>
+        <div class="wgc-box">
+            <div class="header medium">
+                <?php _e( '<h4>Installation</h4>', 'woocommerce-gift-coupon' ); ?>
+            </div>
+            <div class="wgc-box-body">
+                <?php echo '<ul>'; ?>
+                    <?php _e( '<li>Download the form from :</li>', 'woocommerce-gift-coupon' ); ?>
+                    <?php _e( '<li>Upload the downloaded via FTP to / wp-content/plugins folder</li>' , 'woocommerce-gift-coupon' ); ?>
+                    <?php _e( '<li>Activate the module from your management panel plugins</li>', 'woocommerce-gift-coupon' ); ?>
+                <?php echo '</ul>'; ?>
+            </div>
+        </div>
+        <div class="wgc-box">
+            <div class="header medium">
+                <?php _e( '<h4>How to use?</h4>', 'woocommerce-gift-coupon' ); ?>
+            </div>
+            <div class="wgc-box-body">
+                <?php echo '<ul>'; ?>
+                    <?php _e( '<li>This module Woocomerce creates a category called GiftCoupon and assigned a test product)</li>', 'woocommerce-gift-coupon' ); ?>
+                    <?php _e( '<li>Create a new product and check GiftCoupon category, after, configuré your coupon in the details page product.</li>', 'woocommerce-gift-coupon' ); ?>
+                    <?php _e( '<li>When a customer buys a product, we will order and we see it in the list of orders WooCommerce</li>', 'woocommerce-gift-coupon' ); ?>
+                    <?php _e( '<li>We may difference if the order is of type coupon , if so the administrator can select all the completed orders and generate them a new coupon equivalent to the price of product WooCommerce. Automatically coupon will be mailed to the customer</li>', 'woocommerce-gift-coupon' ); ?>
+                    <?php _e( '<li>The plugin has a configuration that will allow us to configure our email template</li>', 'woocommerce-gift-coupon' ); ?>
+                <?php echo '</ul>'; ?>
+            </div>
+        </div>
+    </div>
+</div> <!-- container -->
+
